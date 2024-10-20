b148b1d9f4efc5ed2b4df1ffb90ac96ecdb8491e
JeissonBarrantes <jeisson.barrantes@udea.edu.co>
2024-10-20 10:52:10 -0500

    ApiAuthApplicationTests update configuration setUp()

--- src/test/java/com/authb/api_auth/ApiAuthApplicationTests.java
+++ src/test/java/com/authb/api_auth/ApiAuthApplicationTests.java
@@ -8,6 +8,7 @@ import com.authb.api_auth.repository.RoleRepository;
 import com.authb.api_auth.repository.UserRepository;
 import com.authb.api_auth.service.UserService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,35 @@ class ApiAuthApplicationTests {
 	RoleRepository roleRepository;
 	private User user;
 
+	@BeforeEach
+	void setUp() {
+		UserDto userDto = new UserDto(
+				null, // id se generará automáticamente
+				1, // idType: proporciona un valor válido
+				1, // city: proporciona un valor válido
+				1, // gender: proporciona un valor válido
+				1, // role: proporciona un valor válido (asegúrate de que exista en la DB)
+				"123456789", // identificationNumber: un valor de prueba
+				"Santiago", // firstName
+				"Correa", // lastName
+				"2000-05-07", // birthDate
+				"1234567890", // phoneNumber: un número de teléfono válido
+				"prueba1@udea.edu.co", // email: un email de prueba
+				"password123", // password: la contraseña que quieres usar
+				null, // avatarUrl
+				"Address 123" // address
+		);
+
+		user = userResolver.signUp(userDto);
+	}
+	@AfterEach
+	void cleanUp(){
+		if (user != null) {
+			userRepository.delete(user);
+		}
+	}
+
+
 	@Test
 	void successfulSingUp(){
 		UserDto userDto = new UserDto( null,1,1,1,1,"1","santiago", "correa"
@@ -35,12 +65,6 @@ class ApiAuthApplicationTests {
 		assertEquals(userDto.getEmail(), user.getEmail());
 
 		userRepository.delete(user);
-	}
-	@AfterEach
-	void cleanUp(){
-		if (user != null) {
-			userRepository.delete(user);
-		}
 	}
 
 	@Test
